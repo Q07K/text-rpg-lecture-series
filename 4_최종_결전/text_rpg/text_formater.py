0b2ec8684b77434a8f36e8c4dee98bb11422015a
Q07K <kgh0730@gmail.com>
2025-04-09 15:26:13 +0900

    Refactor: 중복된 코드 분리리

--- 4_최종_결전/text_rpg/text_formater.py
+++ 4_최종_결전/text_rpg/text_formater.py
@@ -1,10 +1,15 @@
 """텍스트 포맷 관리 모듈"""
 
 
-def menu_box(title, data):
-    title = f"[ {title} ]"
-    title = f"┌{title:─^50}┐"
+def make_box_title(title):
+    title_str = f"[ {title} ]"
+    top = f"┌{title_str:─^50}┐"
     bottom = f"└{'':─^50}┘"
+    return top, bottom
+
+
+def menu_box(title, data):
+    top, bottom = make_box_title(title)
 
     result = ""
     for key, value in data.items():
@@ -12,13 +17,11 @@ def menu_box(title, data):
         line = f"│{line:<50}│"
         result += line + "\n"
 
-    return title + "\n" + result + bottom
+    return top + "\n" + result + bottom
 
 
 def menu_box_num(title, data):
-    title = f"[ {title} ]"
-    title = f"┌{title:─^50}┐"
-    bottom = f"└{'':─^50}┘"
+    top, bottom = make_box_title(title)
 
     result = ""
     for idx, value in enumerate(data):
@@ -26,4 +29,4 @@ def menu_box_num(title, data):
         line = f"│{line:<50}│"
         result += line + "\n"
 
-    return title + "\n" + result + bottom
+    return top + "\n" + result + bottom
